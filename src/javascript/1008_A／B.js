cf9c72a4095055abe8ae3a400cc47872d9207dda
cruelladevil <dev.timetravel@gmail.com>
2023-11-07 01:04:31 +0900

    add(javascript): 1008번: A/B

---
+++ src/javascript/1008_A／B.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split(' ').map(Number);
+const [a, b] = input;
+
+const divide = (a, b) => a / b;
+
+console.log(divide(a, b));
