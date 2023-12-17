81717eb42d7ec298633490d6d1756c90ed743982
cruelladevil <dev.timetravel@gmail.com>
2023-12-18 01:27:53 +0900

    add(javascript): 7576번: 토마토

---
+++ src/javascript/7576_토마토.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin', 'utf-8')
+  .trim()
+  .split(/\r?\n/)
+  .map((string) => string.split(' ').map(Number));
+const [[m, n], ...matrix] = input;
+
+const RIPPED_TOMATO = 1;
+const TOMATO = 0;
+
+const directions = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+const ripen = () => {
+  const queue = [];
+  let minDay = 0;
+
+  for (let row = 0; row < n; row += 1) {
+    for (let col = 0; col < m; col += 1) {
+      if (matrix[row][col] === RIPPED_TOMATO) {
+        queue.push([[row, col], 0]);
+      }
+    }
+  }
+
+  for (let i = 0; i < queue.length; i += 1) {
+    const [[row, col], day] = queue[i];
+
+    directions.forEach(([dRow, dCol]) => {
+      const newRow = row + dRow;
+      const newCol = col + dCol;
+      const isInMatrix = newRow >= 0 && newRow < n && newCol >= 0 && newCol < m;
+
+      if (isInMatrix && matrix[newRow][newCol] === TOMATO) {
+        queue.push([[newRow, newCol], day + 1]);
+        matrix[newRow][newCol] = RIPPED_TOMATO;
+        minDay = day + 1;
+      }
+    });
+  }
+
+  for (let row = 0; row < n; row += 1) {
+    for (let col = 0; col < m; col += 1) {
+      if (matrix[row][col] === TOMATO) {
+        return -1;
+      }
+    }
+  }
+
+  return minDay;
+};
+
+console.log(ripen());
