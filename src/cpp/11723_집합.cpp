532ae6e04fabf6265232e33ea63763b1659e8ea3
cruelladevil <dev.timetravel@gmail.com>
2023-12-22 20:51:00 +0900

    add(cpp): 11723번: 집합

---
+++ src/cpp/11723_집합.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  const std::string ADD = "add";
+  const std::string REMOVE = "remove";
+  const std::string CHECK = "check";
+  const std::string TOGGLE = "toggle";
+  const std::string ALL = "all";
+  const std::string EMPTY = "empty";
+
+  int M;
+  int val;
+  int bit = 0;
+  std::string action;
+
+  std::cin >> M;
+
+  for (int i = 0; i < M; i += 1) {
+    std::cin >> action;
+
+    if (action == ADD) {
+      std::cin >> val;
+
+      bit |= (1 << val);
+    } else if (action == REMOVE) {
+      std::cin >> val;
+
+      bit &= ~(1 << val);
+    } else if (action == CHECK) {
+      std::cin >> val;
+
+      if (bit & (1 << val)) {
+        std::cout << 1 << '\n';
+      } else {
+        std::cout << 0 << '\n';
+      }
+    } else if (action == TOGGLE) {
+      std::cin >> val;
+
+      bit ^= (1 << val);
+    } else if (action == ALL) {
+      bit = (1 << 21) - 1;
+    } else if (action == EMPTY) {
+      bit = 0;
+    }
+  }
+}
