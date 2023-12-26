e4906adce83ad971f9698b38afe4b84d49ce362d
cruelladevil <dev.timetravel@gmail.com>
2023-12-27 00:03:27 +0900

    add(javascript): 11022번: A+B - 8

---
+++ src/javascript/11022_A+B_-_8.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin', 'utf-8')
+  .trim()
+  .split(/\r?\n/)
+  .slice(1)
+  .map((string) => string.split(' ').map(Number));
+
+console.log(input.map(([a, b], i) => `Case #${i + 1}: ${a} + ${b} = ${a + b}`).join('\n'));
